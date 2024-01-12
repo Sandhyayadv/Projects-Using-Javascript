ce196e7d6db249dcf44a87ed941b17e8ff820c1d
Sandhyayadv <111213114+Sandhyayadv@users.noreply.github.com>
2024-01-12 22:13:33 +0530

    Add files via upload

---
+++ Projects_On_JavaScript/GitHub-Users/GitHub_Users.js
@@ -0,0 +1,55 @@
+// https://api.github.com/users
+
+// using fetch method
+function githubUser() {
+  fetch("https://api.github.com/users")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      getdata(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+getdata = (data) => {
+  let display = "";
+  data.map((values) => {
+    display += `
+    <div class="cards">
+        <img src=${values.avatar_url} alt=${values.login}>
+        <h1>${values.login}</h1>
+    </div>
+    `;
+  });
+  document.getElementById("root").innerHTML = display;
+};
+githubUser();
+
+// using async await
+
+// async function githubUser(){
+// let response=await fetch('https://api.github.com/users')
+// console.log(response);
+// let data=await response.json();
+// console.log(data);
+// console.log(data[0].avatar_url);
+// let display="";
+// data.map((values)=>{
+//      display +=`
+//     <div class="cards">
+//         <img src=${values.avatar_url} alt=${values.login}>
+//         <h1>${values.login}</h1>
+//     </div>
+//     `
+// })
+// document.getElementById("root").innerHTML=display;
+
+// await new Promise((resolve,reject)=>{
+//     setTimeout(resolve,2000);
+// })
+// console.log("All data is been displayed ")
+// }
+// githubUser();
